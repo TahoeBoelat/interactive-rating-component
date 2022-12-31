7bcb3df4c07c1e58601ed7a365d258cad61f1ebe
TahoeBoelat <muhammadnuralfi98@gmail.com>
2022-12-31 14:16:21 +0700

    Adding elements in HTML, and CSS and Initialing JS variable

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialing variable
+const webIcon = document.querySelector('#iconWeb')
+const webTitle = document.querySelector('#titleWeb')
+const webParagraph = document.querySelector('#paragraphWeb')
+const webNotifContainer = document.querySelector('#webNotifContainer')
+const newValue = {
+    newWebTitle: 'Thank You!',
+    newWebParagraph: 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!'
+}
+// Web Logic
+console.log(webTitle)
